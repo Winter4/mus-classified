179d30a8a349fcc246d1a8fc39b272118fd57dea
moongodhax <69595918+moongodhax@users.noreply.github.com>
2023-01-19 19:29:40 +0300

    fix await is only valid in async functions and the top level bodies of modules

--- api/models/index.js
+++ api/models/index.js
@@ -11,12 +11,9 @@ let sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, proc
   logging: false,
 });
 
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
+sequelize.authenticate()
+.then(() => { console.log('Connection has been established successfully.'); })
+.catch((error) => { console.error('Unable to connect to the database:', error); })
 
 const db = {};
 
