ecca9c5d28c7c0ecd3415b5397ca0a4d283d6a94
moongodhax <69595918+moongodhax@users.noreply.github.com>
2023-01-23 02:23:51 +0300

    show ads on main

---
+++ client/src/stores/advertisements.js
@@ -0,0 +1,20 @@
+import { defineStore } from "pinia";
+import apiRequest from '@/helpers/apiRequest';
+
+export const useAdvertisementsStore = defineStore("advertisements", {
+  state: () => ({
+    ads: [],
+  }),
+
+  actions: {
+    async loadAds(offset = 0, count = 20) {
+      let response = await apiRequest(
+        "/ads/getAll?" + new URLSearchParams({ offset, count }), 
+        { method: 'GET', }
+      );
+
+      this.ads = await response.json();
+      let a = 1;
+    },
+  },
+});
---
+++ client/src/views/MainView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container-lg">
+    <div class="row">
+
+      <div 
+        v-for="ad in advertisements"
+        class="col-3"
+      >
+        <div class="card">
+          <img :src="imageBaseUrl + ad.Images[0].path" class="card-img-top">
+          <div class="card-body">
+            <h6 class="card-title">{{ ad.headline }}</h6>
+            <h6 class="card-subtitle text-muted">{{ ad.price }}₽</h6>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import { useAdvertisementsStore } from "@/stores/advertisements";
+
+export default {
+  setup() {
+    let advertisementsStore = useAdvertisementsStore();
+    advertisementsStore.loadAds();
+
+    return { 
+      advertisementsStore,
+      imageBaseUrl: `${import.meta.env.VITE_API_URL}/upload/images/`,
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    advertisements() {
+      return this.advertisementsStore.ads;
+    }
+  },
+  methods: {
+  }
+}
+</script>
