85f14d61041278f03d4db620672e77844a295c14
moongodhax <69595918+moongodhax@users.noreply.github.com>
2023-06-09 14:01:41 +0300

    fix request doesnt end

--- api/middleware/requiresAdmin.js
+++ api/middleware/requiresAdmin.js
@@ -2,5 +2,5 @@ const userRoles = require("../helpers/userRoles");
 
 module.exports = (req, res, next) => {
   if (req.currentUser.role == userRoles.ADMIN) return next();
-  else return res.status(401);
+  else return res.status(401).json();
 }
