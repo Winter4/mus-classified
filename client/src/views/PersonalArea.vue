0eccfb3136b9f137c240569e8fd42686baaca6e0
moongodhax <69595918+moongodhax@users.noreply.github.com>
2023-06-17 02:58:18 +0300

    отчет в личном кабинете

--- client/src/views/PersonalArea.vue
+++ client/src/views/PersonalArea.vue
@@ -1,6 +1,61 @@
 <template>
   <h4>Личный кабинет</h4>
+
+  <div class="row">
+    <div class="col-4">
+      Пользователь:
+    </div>
+    <div class="col-8">
+      {{ userStore.user.firstName }} {{ userStore.user.lastName }} 
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-4">
+      Объявлений:
+    </div>
+    <div class="col-8">
+      5
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-4">
+      Общее количество просмотров:
+    </div>
+    <div class="col-8">
+      356
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-4">
+      Количество успешных сделок:
+    </div>
+    <div class="col-8">
+      3
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-4">
+      Сумма успешных сделок:
+    </div>
+    <div class="col-8">
+      56000₽
+    </div>
+  </div>
 </template>
 
-<script setup>
+<script>
+import { useUserStore } from "@/stores/user";
+
+export default {
+  setup() {
+    const userStore = useUserStore();
+    return { 
+      userStore 
+    };
+  }
+}
 </script>
