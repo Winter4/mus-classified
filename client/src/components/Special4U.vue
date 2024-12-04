4aaf55829396842effb6e4de1bf6c63b4ef846cb
Arthur Kachmazov <arthurkachmazov@yandex.ru>
2024-12-04 16:12:13 +0300

    "Пресонализированные рекомендации"

---
+++ client/src/components/Special4U.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="col-4 mb-4" v-for="ad in specialAds" :key="ad.id">
+    <Advertisement :ad />
+  </div>
+</template>
+
+<script setup>
+import Advertisement from "@/components/Advertisement.vue";
+import { computed } from "vue";
+
+const props = defineProps(["ads"]);
+
+const specialAds = computed(() => {
+  return [...props.ads].sort(() => 0.5 - Math.random()).slice(0, 3);
+});
+</script>
+
+<style lang="scss" scoped></style>
--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -1,35 +1,38 @@
 <template>
   <div class="container-lg">
     <Search />
-    
-    <div class="row mt-3">
 
+    <div class="row mt-3">
       <div class="col-3">
         <SidebarCategories />
         <SidebarManufacturers />
       </div>
 
       <div class="col-9">
-        <h3 class="mt-2 mb-3">Все объявления</h3>
-        
+        <div class="row" v-if="userStore.user">
+          <h3 class="mt-2 mb-3">Специально для вас!</h3>
+          <Special4U :ads="advertisements" />
+        </div>
         <div class="row">
+          <h3 class="mt-2 mb-3">Все объявления</h3>
           <div class="col-4 mb-4" v-for="ad in advertisements">
             <Advertisement :ad="ad" />
           </div>
         </div>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
-import Search from '@/components/Search.vue';
-import SidebarCategories from '@/components/SidebarCategories.vue';
-import SidebarManufacturers from '@/components/SidebarManufacturers.vue';
-import Advertisement from '@/components/Advertisement.vue';
+import Search from "@/components/Search.vue";
+import SidebarCategories from "@/components/SidebarCategories.vue";
+import SidebarManufacturers from "@/components/SidebarManufacturers.vue";
+import Advertisement from "@/components/Advertisement.vue";
 
 import { useAdvertisementsStore } from "@/stores/advertisements";
+import { useUserStore } from "@/stores/user";
+import Special4U from "@/components/Special4U.vue";
 
 export default {
   components: {
@@ -37,20 +40,24 @@ export default {
     SidebarCategories,
     SidebarManufacturers,
     Advertisement,
+    Special4U,
   },
   setup() {
     let advertisementsStore = useAdvertisementsStore();
     advertisementsStore.getAll();
 
-    return { 
+    let userStore = useUserStore();
+
+    return {
       advertisementsStore,
+      userStore,
       imageBaseUrl: `${import.meta.env.VITE_API_URL}/upload/images/`,
-    }
+    };
   },
   computed: {
     advertisements() {
       return this.advertisementsStore.ads;
-    }
+    },
   },
-}
+};
 </script>
