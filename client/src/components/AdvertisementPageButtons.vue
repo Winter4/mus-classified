68ead8ec60f3d11f81595c6a4fcee81d46e2237f
Arthur Kachmazov <arthurkachmazov@yandex.ru>
2024-12-04 14:40:59 +0300

    Кнопка "Написать сообщение" для неавторизованных

--- client/src/components/AdvertisementPageButtons.vue
+++ client/src/components/AdvertisementPageButtons.vue
@@ -1,44 +1,85 @@
 <template>
   <template v-if="userStore.user">
     <template v-if="ad?.User?.id == userStore.user.id">
-      <RouterLink 
-        :to="{ name: 'advertisementEdit', params: { id: ad.id } }" 
-        class="btn btn-primary w-100 mb-3">
+      <RouterLink
+        :to="{ name: 'advertisementEdit', params: { id: ad.id } }"
+        class="btn btn-primary w-100 mb-3"
+      >
         Редактировать объявление
       </RouterLink>
 
-      <button type="button" class="btn btn-outline-danger w-100" @click="removeAd()">
+      <button
+        type="button"
+        class="btn btn-outline-danger w-100"
+        @click="removeAd()"
+      >
         Удалить объявление
       </button>
     </template>
 
-    <template v-else-if="userStore.user.role == userRoles.MODER && ad.moderStatus == adModerStatus.CREATED">
-      <button type="button" class="btn btn-primary w-100 mb-3" @click="publish()">
+    <template
+      v-else-if="
+        userStore.user.role == userRoles.MODER &&
+        ad.moderStatus == adModerStatus.CREATED
+      "
+    >
+      <button
+        type="button"
+        class="btn btn-primary w-100 mb-3"
+        @click="publish()"
+      >
         Опубликовать объявление
       </button>
 
-      <button type="button" class="btn btn-outline-danger w-100" @click="refuse()">
+      <button
+        type="button"
+        class="btn btn-outline-danger w-100"
+        @click="refuse()"
+      >
         Отказать в публикации
       </button>
     </template>
 
-    <template v-else-if="userStore.user.role == userRoles.EXPERT && ad.expertStatus == adExpertStatus.REQUESTED">
+    <template
+      v-else-if="
+        userStore.user.role == userRoles.EXPERT &&
+        ad.expertStatus == adExpertStatus.REQUESTED
+      "
+    >
       <button type="button" class="btn btn-primary w-100" @click="setReview()">
         Оставить экспертную оценку
       </button>
     </template>
 
-    <template v-else-if="userStore.user.role == userRoles.USER && ad?.User?.id != userStore.user.id">
-      <button type="button" class="btn btn-primary w-100 mb-3" data-bs-toggle="modal" data-bs-target="#messagesModal">
+    <template
+      v-else-if="
+        userStore.user.role == userRoles.USER &&
+        ad?.User?.id != userStore.user.id
+      "
+    >
+      <button
+        type="button"
+        class="btn btn-primary w-100 mb-3"
+        data-bs-toggle="modal"
+        data-bs-target="#messagesModal"
+      >
         Написать сообщение
       </button>
 
       <MessagesModal v-if="ad?.User?.id" :userId="ad?.User?.id" />
 
-      <button v-if="ad?.expertStatus == adExpertStatus.CREATED" type="button" class="btn btn-primary w-100" @click="requestReview()">
+      <button
+        v-if="ad?.expertStatus == adExpertStatus.CREATED"
+        type="button"
+        class="btn btn-primary w-100"
+        @click="requestReview()"
+      >
         Запросить экспертную оценку
       </button>
-      <div v-else-if="ad?.expertStatus == adExpertStatus.REQUESTED" class="card text-white bg-success">
+      <div
+        v-else-if="ad?.expertStatus == adExpertStatus.REQUESTED"
+        class="card text-white bg-success"
+      >
         <div class="card-body">
           Экспертная оценка объявления скоро появится!
         </div>
@@ -48,20 +89,31 @@
 
   <div v-else class="card">
     <div class="card-body">
-      Зарегистрируйтесь или войдите в систему, чтобы написать сообщение продавцу или запросить экспертную оценку объявления.
+      Зарегистрируйтесь или войдите в систему, чтобы написать сообщение продавцу
+      или запросить экспертную оценку объявления.
+      <button
+        type="button"
+        class="btn btn-primary w-100 mb-3"
+        data-bs-toggle="modal"
+        data-bs-target="#messagesModal"
+      >
+        Написать сообщение
+      </button>
+
+      <MessagesModal v-if="ad?.User?.id" :userId="{}" />
     </div>
   </div>
 </template>
 
 <script>
-import { RouterLink } from 'vue-router';
-import MessagesModal from '@/components/MessagesModal.vue';
+import { RouterLink } from "vue-router";
+import MessagesModal from "@/components/MessagesModal.vue";
 import { useUserStore } from "@/stores/user";
 import userRoles from "@/helpers/userRoles";
 import adModerStatus from "@/helpers/adModerStatus";
 import adExpertStatus from "@/helpers/adExpertStatus";
 import { useAdvertisementsStore } from "@/stores/advertisements";
-import router from '@/router';
+import router from "@/router";
 
 export default {
   props: ["ad"],
@@ -72,8 +124,7 @@ export default {
   setup() {
     const userStore = useUserStore();
     let advertisementsStore = useAdvertisementsStore();
-    
-    return { 
+    return {
       userStore,
       userRoles,
       advertisementsStore,
@@ -91,27 +142,38 @@ export default {
     async setReview() {
       if (confirm("Вы действительно хотите оставить экспертную оценку?")) {
         let review = prompt("Введите текст экспертной оценки: ");
-        await this.advertisementsStore.editExpert(this.ad.id, adExpertStatus.REVIEWED, review);
+        await this.advertisementsStore.editExpert(
+          this.ad.id,
+          adExpertStatus.REVIEWED,
+          review
+        );
         this.$router.go();
       }
     },
     async publish() {
       if (confirm("Вы действительно хотите опубликовать объявление?")) {
-        await this.advertisementsStore.editModer(this.ad.id, adModerStatus.PUBLISHED);
+        await this.advertisementsStore.editModer(
+          this.ad.id,
+          adModerStatus.PUBLISHED
+        );
       }
     },
     async refuse() {
       if (confirm("Вы действительно хотите отказать в публикации?")) {
         let reason = prompt("Введите причину: ");
-        await this.advertisementsStore.editModer(this.ad.id, adModerStatus.REFUSED, reason);
+        await this.advertisementsStore.editModer(
+          this.ad.id,
+          adModerStatus.REFUSED,
+          reason
+        );
       }
     },
     async removeAd() {
       if (confirm("Вы действительно хотите удалить объявление?")) {
         await this.advertisementsStore.remove(this.ad.id);
-        router.push({ name: 'personalAds' });
+        router.push({ name: "personalAds" });
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
--- client/src/components/MessagesModal.vue
+++ client/src/components/MessagesModal.vue
@@ -4,28 +4,43 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title">Сообщения</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
         <div class="modal-body">
-
-          <div 
-            v-for="msg in messages"
-            style="overflow: hidden;"
-          >
-            <div 
-              :class="'p-2 border rounded mb-2 border-secondary ' + (msg.fromId == userId ? 'float-start' : 'float-end') ">
+          <div v-for="msg in messages" :key="msg.id" style="overflow: hidden">
+            <div
+              :class="
+                'p-2 border rounded mb-2 border-secondary ' +
+                (msg.fromId == userId ? 'float-start' : 'float-end')
+              "
+            >
               {{ msg.text }}
             </div>
           </div>
-          
         </div>
         <div class="modal-footer d-block">
           <div class="row">
             <div class="col-10">
-              <input v-model="message" type="text" class="form-control" placeholder="Введите сообщение">
+              <input
+                v-model="message"
+                type="text"
+                class="form-control"
+                placeholder="Введите сообщение"
+              />
             </div>
             <div class="col-2">
-              <button type="button" class="btn btn-primary" @click="sendMessage()">Отп</button>
+              <button
+                type="button"
+                class="btn btn-primary"
+                @click="sendMessage()"
+              >
+                Отп
+              </button>
             </div>
           </div>
         </div>
@@ -41,36 +56,38 @@ export default {
   props: ["userId"],
   setup() {
     const messageStore = useMessageStore();
-    
-    return { 
+
+    return {
       messageStore,
     };
   },
+
   data() {
     return {
-      message: '',
+      message: "",
       intervalId: null,
-    }
+    };
   },
   computed: {
     messages() {
-      return this.messageStore.messages;
-    }
+      return this.messageStore.messages || [];
+    },
   },
   methods: {
     async sendMessage() {
-      await this.messageStore.add(this.userId, this.message);
-      this.message = '';
-      this.messageStore.getAll(this.userId);
-    }
+      if (this.userId) {
+        await this.messageStore.add(this.userId, this.message);
+        this.message = "";
+        this.messageStore.getAll(this.userId);
+      }
+    },
   },
   created() {
-    this.messageStore.getAll(this.userId);
-
-    clearInterval(this.intervalId);
-    this.intervalId = setInterval(() => {
-      this.messageStore.getAll(this.userId);
-    }, 500);
+    // this.messageStore.getAll(this.userId);
+    // clearInterval(this.intervalId);
+    // this.intervalId = setInterval(() => {
+    //   this.messageStore.getAll(this.userId);
+    // }, 500);
   },
-}
+};
 </script>
