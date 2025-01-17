4cd94dcf32adc1eaac17cad5d697ba2b0751ae36
Arthur Kachmazov <arthurkachmazov@yandex.ru>
2025-01-17 23:16:25 +0300

    Метрика

--- client/src/components/MessagesModal.vue
+++ client/src/components/MessagesModal.vue
@@ -4,28 +4,43 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title">Сообщения</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
         <div class="modal-body">
-
-          <div 
-            v-for="msg in messages"
-            style="overflow: hidden;"
-          >
-            <div 
-              :class="'p-2 border rounded mb-2 border-secondary ' + (msg.fromId == userId ? 'float-start' : 'float-end') ">
+          <div v-for="msg in messages" style="overflow: hidden">
+            <div
+              :class="
+                'p-2 border rounded mb-2 border-secondary ' +
+                (msg.fromId == userId ? 'float-start' : 'float-end')
+              "
+            >
               {{ msg.text }}
             </div>
           </div>
-          
         </div>
         <div class="modal-footer d-block">
           <div class="row">
             <div class="col-10">
-              <input v-model="message" type="text" class="form-control" placeholder="Введите сообщение">
+              <input
+                v-model="message"
+                type="text"
+                class="form-control"
+                placeholder="Введите сообщение"
+              />
             </div>
             <div class="col-2">
-              <button type="button" class="btn btn-primary" @click="sendMessage()">Отп</button>
+              <button
+                type="button"
+                class="btn btn-primary"
+                @click="sendMessage()"
+              >
+                Отп
+              </button>
             </div>
           </div>
         </div>
@@ -41,28 +56,29 @@ export default {
   props: ["userId"],
   setup() {
     const messageStore = useMessageStore();
-    
-    return { 
+
+    return {
       messageStore,
     };
   },
   data() {
     return {
-      message: '',
+      message: "",
       intervalId: null,
-    }
+    };
   },
   computed: {
     messages() {
       return this.messageStore.messages;
-    }
+    },
   },
   methods: {
     async sendMessage() {
+      window.ym(98709910, "reachGoal", " hypothesis5");
       await this.messageStore.add(this.userId, this.message);
-      this.message = '';
+      this.message = "";
       this.messageStore.getAll(this.userId);
-    }
+    },
   },
   created() {
     this.messageStore.getAll(this.userId);
@@ -72,5 +88,5 @@ export default {
       this.messageStore.getAll(this.userId);
     }, 500);
   },
-}
+};
 </script>
